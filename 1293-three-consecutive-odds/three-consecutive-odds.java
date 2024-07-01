11c9a627a27ee7759dbe5daab5a443eb6859abfa
Himanshu Shekhar Panda <123072158+PandaHimanshu@users.noreply.github.com>
2024-07-01 08:03:30 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (96.91%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int streak = 0;
+        for(int i:arr){
+            if(i%2!=0){
+                streak++;
+                if(streak==3){
+                    return true;
+                }
+            }else{
+                streak = 0;
+            }
+        }
+        return false;
+    }
+}
