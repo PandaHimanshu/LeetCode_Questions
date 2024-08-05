5ce11b6535dffbb8f74f5e10f3fc0b5530dd48d4
Himanshu Shekhar Panda <123072158+PandaHimanshu@users.noreply.github.com>
2024-08-05 22:25:23 +0530

    Time: 6 ms (91.98%) | Memory: 44.2 MB (35.94%) - LeetSync

---
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        Map<String, Integer> countMap = new HashMap<>();
+        for (String s : arr) {
+            countMap.put(s, countMap.getOrDefault(s, 0) + 1);
+        }
+        int distinctCount = 0;
+        for (String s : arr) {
+            if (countMap.get(s) == 1) {
+                distinctCount++;
+                if (distinctCount == k) {
+                    return s;
+                }
+            }
+        }
+
+        // Step 3: If there are fewer than k distinct strings, return an empty string
+        return "";
+    }
+}
