ce0e661c2bb07d8a23d5b50f88ae7f2d775600c4
Himanshu Shekhar Panda <123072158+PandaHimanshu@users.noreply.github.com>
2025-01-21 23:17:07 +0530

    Time: 4 ms (17.50%) | Memory: 63.3 MB (77.74%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
@@ -16,9 +16,16 @@ class Solution {
             return null;
         }
 
-        while(fast.next.next != null && fast.next.next.next != null){
-            slow=slow.next;
-            fast=fast.next.next;
+        // while(fast.next.next != null && fast.next.next.next != null){
+        //     slow=slow.next;
+        //     fast=fast.next.next;
+        // }
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            if (fast != null && fast.next != null) {
+                slow = slow.next;
+            }
         }
         slow.next=slow.next.next;
         slow=head;
